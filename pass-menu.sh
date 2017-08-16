9eb32f1371f2fa04db5e7fef56bd6e508e7e4552
denimor <denimor@bk.ru>
2017-08-16 16:47:51 +0300

    Fixed problems with special characters in XML

--- pass-menu.sh
+++ pass-menu.sh
@@ -1,41 +1,49 @@
 #!/usr/bin/env bash
 
-PREFIX="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
-PS_PATH="$1"
+escape() {
+	local s=${1//\&/&amp;}
+	s=${s//\'/&apos;}
+	echo "$s"
+}
 
-#~ TYPE_CMD='xvkbd -file - 2>/dev/null'
-TYPE_CMD='xdotool type --clearmodifiers --file -'
+gen_xml() {
+	local PREFIX="${PASSWORD_STORE_DIR:-$HOME/.password-store}"
+	local PS_PATH=$(base64 -d <<< "$1")
+	local MENU_ID LABEL OPT
 
-TPL_DIR_MENU="<menu id='pass-%s' label='%s' execute='$0 %q' />\n"
-TPL_FILE_MENU="<item label='Type _password'>
-	<action name='Execute'><command>
-		sh -c 'pass show -- %q | $TYPE_CMD'
-	</command></action>
-</item>
-<item label='Type _login and password'>
-	<action name='Execute'><command>
-		sh -c '(echo -ne %q ; pass show -- %q) | $TYPE_CMD'
-	</command></action>
-</item>
-<item label='_Copy password to clipboard'>
-	<action name='Execute'><command>
-		pass show -c -- %q ;
-	</command></action>
-</item>\n"
+	echo '<?xml version="1.0" encoding="UTF-8"?><openbox_pipe_menu>'
+
+	if [[ -d $PREFIX/$PS_PATH ]] ; then
+		cd -- "$PREFIX/$PS_PATH"
+		[[ $PS_PATH ]] && PS_PATH="$PS_PATH/"
+		for item in * ; do
+			LABEL=${item%.gpg}
+			OPT=$(echo -n "${PS_PATH}${LABEL}" | base64)
+			LABEL=$(escape "$LABEL")
+			echo "<menu id='$OPT' label='$LABEL' execute='$0 $OPT' />"
+		done
+	else
+		printf "<item label='%s'><action name='Execute'><command>$0 %s $1</command></action></item>\n" \
+			'Type _password' type \
+			'Type _login and password' type_ex \
+			'Copy password to _clipboard' clip
+	fi
+
+	echo '</openbox_pipe_menu>'
+}
 
 
-echo '<?xml version="1.0" encoding="UTF-8"?><openbox_pipe_menu>'
+if [[ $2 ]] ; then
+	PS_PATH=$(base64 -d <<< "$2")
 
-if [ -d "$PREFIX/$PS_PATH" ] ; then
-	cd -- "$PREFIX/$PS_PATH"
-	[ "$PS_PATH" ] && PS_PATH="$PS_PATH/"
-	for item in * ; do
-		printf "$TPL_DIR_MENU" "${PS_PATH}${item}" "${item%.gpg}" "${PS_PATH}${item}"
-	done
+	#~ TYPE_CMD='xvkbd -file - 2>/dev/null'
+	TYPE_CMD='xdotool type --clearmodifiers --file -'
+
+	case "$1" in
+		   type) pass show -- "$PS_PATH" | $TYPE_CMD ;;
+		type_ex) (echo -n "${PS_PATH##*/}	" ; pass show -- "$PS_PATH") | $TYPE_CMD ;;
+			clip) pass show -c -- "$PS_PATH" ;;
+	esac
 else
-	PS_NAME="${PS_PATH%.gpg}"
-	LABEL="${PS_NAME##*/}"
-	printf "$TPL_FILE_MENU" "$PS_NAME" "$LABEL\t" "$PS_NAME" "$PS_NAME"
+	gen_xml "$1"
 fi
-
-echo '</openbox_pipe_menu>'
